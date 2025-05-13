a90eafd3cb03b53458050368f6e3d09e71a07152
dassinehamichee@gmail.com <dassinehamichee@gmail.com>
2025-05-13 12:20:55 +0000

    int_sales_margin.sql

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,22 @@
+#int_sales_margin.sql
+
+WITH sales_data AS (
+    SELECT 
+        sales.orders_id,
+        sales.quantity,
+        sales.revenue,
+        products.purchase_price
+    FROM 
+        {{ ref("stg_raw__sales") }} AS sales
+    LEFT JOIN 
+        {{ ref("stg_raw__product") }} AS products
+    ON 
+        sales.product_id = products.products_id
+)
+
+SELECT
+    orders_id,
+    CAST(quantity AS INT64) * CAST(purchase_price AS FLOAT64) AS purchase_cost,   -- Calcul de purchase_cost
+    ROUND(CAST(revenue AS FLOAT64) - (CAST(quantity AS INT64) * CAST(purchase_price AS FLOAT64)),2) AS margin -- Calcul de la marge
+FROM 
+    sales_data
