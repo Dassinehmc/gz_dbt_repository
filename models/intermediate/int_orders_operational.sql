ad54da941befb4315cbec76bd70fea95672bbdfa
dassinehamichee@gmail.com <dassinehamichee@gmail.com>
2025-05-14 10:37:12 +0000

    last attempt

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,14 @@
 # int_orders_operational.sql
 with
     orders_margin_data as (
-        select orders_id, date_date, margin from {{ ref("int_sales_margin") }}
+        select 
+        orders_id, 
+        date_date, 
+        margin,
+        revenue,
+        quantity,
+        purchase_price
+        from {{ ref("int_sales_margin") }}
     ),
 
     shipping_data as (
@@ -16,6 +23,14 @@ with
 select
     orders_margin_data.orders_id,
     orders_margin_data.date_date,
-    round(margin + shipping_fee - log_cost - ship_cost, 2) as operational_margin
+    round(margin + shipping_fee - log_cost - ship_cost, 2) as operational_margin,
+    orders_margin_data.revenue,
+    orders_margin_data.quantity,
+    orders_margin_data.purchase_price,
+    orders_margin_data.margin,
+    shipping_data.shipping_fee,
+    shipping_data.log_cost,          
+    shipping_data.ship_cost  
 from orders_margin_data
-left join shipping_data on orders_margin_data.orders_id = shipping_data.orders_id
+left join shipping_data 
+on orders_margin_data.orders_id = shipping_data.orders_id
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -8,11 +8,10 @@
      ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
      ,ROUND(SUM(margin),0) AS margin
      ,ROUND(SUM(operational_margin),0) AS operational_margin
-     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
+     ,ROUND(SUM(purchase_price),0) AS purchase_cost
      ,ROUND(SUM(shipping_fee),0) AS shipping_fee
-     ,ROUND(SUM(logcost),0) AS logcost
      ,ROUND(SUM(ship_cost),0) AS ship_cost
+     ,ROUND(SUM(log_cost),0) AS logcost
      ,SUM(quantity) AS quantity
  FROM {{ref("int_orders_operational")}}
  GROUP BY  date_date
- ORDER BY  date_date DESC
